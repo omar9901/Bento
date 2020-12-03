3b2d1bd8ce13da04b186e3af237c90f5a140e836
omar9901 <omar.elshazly99@gmail.com>
2020-12-02 20:12:31 -0500

    Update weather.js

--- js/weather.js
+++ js/weather.js
@@ -13,7 +13,7 @@ var tempUnit = 'C';
 
 const KELVIN = 273.15;
 // Use your own key for the Weather, Get it here: https://openweathermap.org/
-const key = 'aa5b0a76dfbf87541928fb3cc32d3d69';
+const key = 'e71d8c1d0e10d7a546700bfa0f9efe28';
 
 // Set Position function
 setPosition();
@@ -21,8 +21,8 @@ setPosition();
 function setPosition(position) {
     // Here you can change your position
     // You can use https://www.latlong.net/ to get it! (I use San Francisco as an example)
-    let latitude = 37.774929;
-    let longitude = -122.419418;
+    let latitude = 43.467518;
+    let longitude = -79.687668;
 
     getWeather(latitude, longitude);
 }
